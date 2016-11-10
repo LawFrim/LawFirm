4baaef1a0eced20bb7078621ca8697bb967f9ef5
liangchaob <liangchaob@163.com>
2016-11-10 21:54:20 +0800

    add amin

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
 
+  namespace :admin do
+    resources :legalmatters do
+      resources :comments
+    end
+  end
 
   resources :legalmatters do
     resources :comments
