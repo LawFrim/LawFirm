23bc8e2f465e08d338258b871ab268846b3ceffb
cndaniel <tigerljt@gmail.com>
2016-11-10 16:27:35 +0800

    remove merge mark in develop

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,8 @@
 class WelcomeController < ApplicationController
-<<<<<<< HEAD
+
   def index
     flash[:notice] = "早安！你好！"
   end
- 
-=======
->>>>>>> 92e539ccf44533e2a252efb950d5b695b34bfa9c
+
+
 end
