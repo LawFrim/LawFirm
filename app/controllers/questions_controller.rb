f035c8cb2bce167c6a2d76c26f1854fc34405cf6
sunyanfei <sunyanfei2016@gmail.com>
2016-11-10 16:52:06 +0800

    inplement questions crud new

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,29 @@
 class QuestionsController < ApplicationController
   def index
-    @questions =Question.all
-  end 
+    @questions = Question.all
+  end
+  def new
+    @question = Question.new
+  end
+
+
+
+  def create
+    @question = Question.new(question_params)
+    @question.save
+    redirect_to question_path
+  end
+
+def show
+  @question = Question.find(params[:id])
+end 
+
+
+
+  private
+
+  def question_params
+    params.require(:question).permit(:description)
+
+  end
 end
