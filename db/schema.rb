d6af26fe0be8a6456112d0b7e7d1dc2d714f6aa5
sunyanfei <sunyanfei2016@gmail.com>
2016-11-14 17:14:42 +0800

    wrong rollback

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161114061115) do
+ActiveRecord::Schema.define(version: 20161111041000) do
 
   create_table "answers", force: :cascade do |t|
     t.text     "content"
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 20161114061115) do
 
   create_table "questions", force: :cascade do |t|
     t.text     "content"
-    t.string   "area"
+    t.string   "type"
     t.string   "district"
     t.integer  "user_id"
     t.datetime "created_at", null: false
@@ -43,9 +43,6 @@ ActiveRecord::Schema.define(version: 20161114061115) do
     t.datetime "created_at",                             null: false
     t.datetime "updated_at",                             null: false
     t.boolean  "is_admin",               default: false
-    t.boolean  "is_lawyer",              default: false
-    t.string   "district"
-    t.string   "area"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -24,9 +24,6 @@ end
 #  created_at             :datetime         not null
 #  updated_at             :datetime         not null
 #  is_admin               :boolean          default(FALSE)
-#  is_lawyer              :boolean          default(FALSE)
-#  district               :string
-#  area                   :string
 #
 # Indexes
 #
