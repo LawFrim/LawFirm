76d20fcda8a94f7f06d973651a91c1662f87635d
liangchaob <liangchaob@163.com>
2016-11-17 22:14:35 +0800

    add a colunm is_answered

--- app/models/question.rb
+++ app/models/question.rb
@@ -17,11 +17,12 @@ end
 #
 # Table name: questions
 #
-#  id         :integer          not null, primary key
-#  content    :text
-#  area       :string
-#  district   :string
-#  user_id    :integer
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id          :integer          not null, primary key
+#  content     :text
+#  area        :string
+#  district    :string
+#  user_id     :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  is_answered :boolean          default(FALSE)
 #
--- test/controllers/questions_controller_test.rb
+++ test/controllers/questions_controller_test.rb
@@ -2,13 +2,14 @@
 #
 # Table name: questions
 #
-#  id         :integer          not null, primary key
-#  content    :text
-#  area       :string
-#  district   :string
-#  user_id    :integer
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id          :integer          not null, primary key
+#  content     :text
+#  area        :string
+#  district    :string
+#  user_id     :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  is_answered :boolean          default(FALSE)
 #
 
 require 'test_helper'
