678661e34a879b1a84796645ac3191efa0079561
sunyanfei <sunyanfei2016@gmail.com>
2016-11-21 21:01:55 +0800

    rake db:rollback

--- db/migrate/20161121124916_create_orders.rb
+++ db/migrate/20161121124916_create_orders.rb
@@ -1,7 +1,10 @@
 class CreateOrders < ActiveRecord::Migration[5.0]
   def change
     create_table :orders do |t|
-
+      t.item :string
+      t.total :string
+      t.billing_name :string
+      t.billing_address :string
       t.timestamps
     end
   end
