f0093b8811e7663b13a4793de52b8ef84f2d250f
sunyanfei <sunyanfei2016@gmail.com>
2016-11-21 18:33:31 +0800

    add account order controller

---
+++ app/controllers/account/orders_controller.rb
@@ -0,0 +1,35 @@
+class Account::OrdersController < ApplicationController
+  before_action :authenticate_user!, only: [:create]#必须注册用户才能建立订单。
+  #按月支付服务费的订单
+  def create_m
+    @order = Order.new(order_params)
+    @order.user = current_user
+    @order.item = "包月法律咨询服务费"
+    @order.total = "999RMB"
+
+    if @order.save
+      redirect_to order_path(@order)
+    else
+      render '/'
+    end
+  end
+  #按年支付服务费的订单
+  def create_y
+    @order = Order.new(order_params)
+    @order.user = current_user
+    @order.item = "包年法律咨询服务费"
+    @order.total = "9999RMB"
+
+    if @order.save
+      redirect_to order_path(@order)
+    else
+      render '/'
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:item,:total,:billing_name, :billing_address)
+  end
+end
