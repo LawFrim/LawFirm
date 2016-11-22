89d51be5aa827ffa9e39ed2722b9446eeabb21fc
sunyanfei <sunyanfei2016@gmail.com>
2016-11-22 19:34:22 +0800

    add admin orders index panel

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
     end
     resources :documents
      resources :feedbacks
-
+     resources :orders
     resources :conversations
     resources :users do
       member do
