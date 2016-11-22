af2d313059f23cff595d4ce1684ae234d3b5ea17
sunyanfei <sunyanfei2016@gmail.com>
2016-11-22 11:48:45 +0800

    add order show html

--- app/controllers/account/orders_controller.rb
+++ app/controllers/account/orders_controller.rb
@@ -2,29 +2,31 @@ class Account::OrdersController < ApplicationController
   before_action :authenticate_user!, only: [:create]#必须注册用户才能建立订单。
   #按月支付服务费的订单
   def create_m
-    @order = Order.new(order_params)
+    @order = Order.new
     @order.user = current_user
     @order.item = "包月法律咨询服务费"
     @order.total = "999"
-
+    @order.billing_name = current_user.order.billing_name
+    @order.billing_address = current_user.order.billing_address
 
     if @order.save
-      redirect_to order_path(@order)
+      redirect_to account_order_path(@order)
     else
-      render '/'
+      redirect_to "/"
     end
   end
   #按年支付服务费的订单
   def create_y
-    @order = Order.new(order_params)
+    @order = Order.new
     @order.user = current_user
     @order.item = "包年法律咨询服务费"
     @order.total = "9999"
 
+
     if @order.save
-      redirect_to order_path(@order)
+      redirect_to account_order_path(@order)
     else
-      render '/'
+      redirect_to "/"
     end
   end
 
@@ -34,7 +36,10 @@ class Account::OrdersController < ApplicationController
 
   def show
     @order = Order.find(params[:id])
+
   end
+
+
 
   def index
     @order = Order.all
