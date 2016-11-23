c009bf019b4154af767dcc87b5092537be2d3830
sunyanfei <sunyanfei2016@gmail.com>
2016-11-23 20:51:02 +0800

    add wrong devisecn

--- config/application.rb
+++ config/application.rb
@@ -12,4 +12,8 @@ module Lawfirm
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
   end
+  config.i18n.default_locale = :"zh-CN"
+  config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**',
+            '*.{rb,yml}').to_s]
+    config.i18n.default_locale = :"zh-CN"
 end
