06dcb6d3e87622e480b7245342656f144813c736
sunyanfei <sunyanfei2016@gmail.com>
2016-11-23 21:29:57 +0800

    add wrong devise

--- config/application.rb
+++ config/application.rb
@@ -6,10 +6,12 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+    config.i18n.default_locale = :"zh-CN"
 module Lawfirm
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
   end
+
 end
