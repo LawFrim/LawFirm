ce4dbe4f3ba0b10f7030ba58ee32da933fd40dad
cndaniel <tigerljt@gmail.com>
2016-11-25 07:49:34 +0800

    remove redudant seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -31,16 +31,6 @@ Question.create!([content:"我能找到真爱么？",area:"民事",district:"北
 Question.create!([content:"我家房子要被政府拆迁了，我真的能实现财富自由么？",area:"经济",district:"北京",user_id:7])
 puts "5 questions created!"
 
-Answer.create!([content:"我觉得你问错人了，你还是申请退钱把。",question_id:1,user_id:2])
-Answer.create!([content:"纳税光荣，敛财于民，敛财为国",question_id:2,user_id:2])
-Answer.create!([content:"我觉得你说的是租房广告，不是合同。广告上最好也不要写，心理暗地这么想就好了",question_id:3,user_id:2])
-Answer.create!([content:"你注定终身孤独。",question_id:4,user_id:2])
-Answer.create!([content:"恩，你就要成为土豪了。我们非常荣幸为您提供法律服务",question_id:5,user_id:2])
-puts "5 answers created!"
+ 
 
-Document.create!([description:"请问这份抢注黄喵的商标注册书合理合法么？",user_id:5])
-Document.create!([description:"我岳父给我的婚前协议书能不能稍微修改下，让我离婚后能够平分财产？",user_id:5])
-Document.create!([description:"这份遗嘱能确保我的巨额遗产不留给我不孝儿女，而是留给照顾我的保姆么？",user_id:5])
-Document.create!([description:"我的新书「北京观察」就要出版了，这是我的出版人黄喵给我的合同，请问有什么需要注意的地方？",user_id:5])
-puts "3 documents offer created!"
 puts "done!"
