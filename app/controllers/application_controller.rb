be58aec4ed2f755289554b420692200b0a00ffa5
sunyanfei <sunyanfei2016@gmail.com>
2016-11-25 11:18:02 +0800

    fix admin login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -28,10 +28,12 @@ class ApplicationController < ActionController::Base
 
     #不同用户登录后跳转到指定页面
     def after_sign_in_path_for(_user)
-      if current_user.is_lawyer?
-        lawyer_questions_path #你的路径
-      elsif current_user.is_admin?
+      if current_user.is_admin?
         admin_users_path
+        current_user.is_lawyer?
+        lawyer_questions_path #你的路径
+      elsif   current_user.is_lawyer?
+        lawyer_questions_path #你的路径
       else
         account_questions_path
       end
