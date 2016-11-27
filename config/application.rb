7d0b3b9e58063dcc1b13561fff1a4abaeda6b504
cndaniel <tigerljt@gmail.com>
2016-11-28 01:35:13 +0800

    application.rb

--- config/application.rb
+++ config/application.rb
@@ -12,9 +12,7 @@ module Lawfirm
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.i18n.default_locale = :"zh-CN"
+    config.serve_static_assets = true
   end
 
 end
-
-
-config.serve_static_assets = true
