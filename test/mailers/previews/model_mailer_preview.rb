7587a2cece752973aa7d8473ba993535ea4a2f32
liangchaob <liangchaob@163.com>
2016-11-29 11:19:09 +0800

    install a mailer

---
+++ test/mailers/previews/model_mailer_preview.rb
@@ -0,0 +1,9 @@
+# Preview all emails at http://localhost:3000/rails/mailers/model_mailer
+class ModelMailerPreview < ActionMailer::Preview
+
+  # Preview this email at http://localhost:3000/rails/mailers/model_mailer/record_notification
+  def record_notification
+    ModelMailer.record_notification
+  end
+
+end
