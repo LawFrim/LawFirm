f27e7c898d061df5b4d9f74077986e73aae524de
sunyanfei <sunyanfei2016@gmail.com>
2016-11-29 13:38:39 +0800

    add user_name partial and admin_index_table partial

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,26 +14,6 @@ module ApplicationHelper
   def time_format(time)
     DateTime.parse(time.to_s).strftime('%Y-%m-%d %H:%M:%S').to_s
   end
-  #用户的身份状态图标
-  def render_user_status(user)
-    if !user.is_admin?
-      if user.is_lawyer?
-        content_tag(:span,"",class:"fa fa-user-plus")
-      else
-        content_tag(:span, "", class: "fa fa-user")
-      end
-    else
-      content_tag(:span,"",class:"fa fa-star-o")
-    end
-  end
 
-  def render_user_vip(user)
-    if !user.is_vip?
-      content_tag(:span,"", class:"fa fa-opera")
-    else
-      content_tag(:span,"", class:"fa fa-vimeo")
-    end
-
-  end
 
 end
