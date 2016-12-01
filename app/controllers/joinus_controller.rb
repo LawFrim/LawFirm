a7bb091eb1a2ce89ffb747e959221b4fbb0ef503
sunyanfei <sunyanfei2016@gmail.com>
2016-12-01 10:36:25 +0800

    delete controller

--- app/controllers/joinus_controller.rb
+++
@@ -1,6 +0,0 @@
-class JoinusController < ApplicationController
-  def show
-  end
-  def index
-  end 
-end
--- test/controllers/mapguide_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class MapguideControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
