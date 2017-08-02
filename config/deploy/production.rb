b9aa44cfcecf47461c30ffe3cc641e8f944c09ba
cnDaniel <cndaniel@me.com>
2017-08-02 16:59:14 +0200

    fix typo in produtcion.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,7 +2,7 @@
 # ======================
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
- set branch, "master"
+set :branch, "master"
  server "172.104.137.49", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
